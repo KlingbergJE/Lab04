74efd30018c81695a2f4fd2273d3b370e34397c0
KlingbergJE <john_klingberg@yahoo.com>
2015-02-13 16:00:01 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,25 +2,18 @@
 cls
 
 set DRIVE_LETTER=%1:
-<<<<<<< HEAD
+
 set PATH=%DRIVE_LETTER%\MinGW\bin;%DRIVE_LETTER%\MinGW\msys\1.0\bin;c:\Windows;c:\Windows\system32
 
-g++ -I.\CSC2110 -c Password.cpp
-g++ -I.\CSC2110 -c PasswordDriver.cpp
+g++  -c CLGUIDriver.cpp
+g++ -c CLGUI.cpp
 
-g++ -L.\CSC2110 -o  Password.exe PasswordDriver.o Password.o -lCSC2110
-Password <input.txt
-
+g++  -o  CLGUIDriver.exe CLGUIDriver.o CLGUI.o 
 
 
 
-
-
-
-
-=======
 set PATH=%DRIVE_LETTER%\MinGW\bin;%DRIVE_LETTER%\MinGW\msys\1.0\bin;%DRIVE_LETTER%\MinGW\gtkmm3\bin;%DRIVE_LETTER%\MinGW\gtk\bin;c:\Windows;c:\Windows\system32
 set PROJECT_PATH=.
 
 make DRIVE_LETTER="%DRIVE_LETTER%" PROJECT_DIR="%PROJECT_PATH%"
->>>>>>> e81b7bd5c28a292b5d91b1f987741b6cb0b8e4cf
+
